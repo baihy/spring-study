64f9b4116d2a73b95e93cd147200af867fe04155
huayang.bai <1053091115@qq.com>
2019-06-03 11:21:19 +0800

    实现了父工程

--- spring-aop-demo/src/main/java/com/baihy/Main.java
+++ spring-aop-demo/src/main/java/com/baihy/Main.java
@@ -1,5 +1,6 @@
 package com.baihy;
 
+import com.baihy.proxy.CglibDynamicImpl;
 import com.baihy.proxy.DynamicImpl;
 import com.baihy.target.IDeveloper;
 import com.baihy.target.impl.Developer;
@@ -14,9 +15,21 @@ import com.baihy.target.impl.Developer;
 public class Main {
 
     public static void main(String[] args) {
+        cglib();
+    }
+
+    public static void jdk() {
         IDeveloper target = new Developer();
         IDeveloper proxy = (IDeveloper) new DynamicImpl().bind(target);
         proxy.develop("baihuayang");
     }
 
+    public static void cglib() {
+        IDeveloper target = new Developer();
+        IDeveloper proxy = (IDeveloper) new CglibDynamicImpl().bind(target);
+        proxy.develop("baihuayang");
+    }
+
+
 }
+
